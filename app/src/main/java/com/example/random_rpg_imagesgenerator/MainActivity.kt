6117f69d29450855f2280b5bc762bb3bea4f12dd
Marcin <marcinzub1@gmail.com>
2022-04-04 20:48:57 +0200

    Add first functionality

--- app/src/main/java/com/example/random_rpg_imagesgenerator/MainActivity.kt
+++ app/src/main/java/com/example/random_rpg_imagesgenerator/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.random_rpg_imagesgenerator
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val images = assets.list("images")
+        val buttonClick = findViewById<Button>(R.id.generate_button)
+        val imageView = findViewById<ImageView>(R.id.image_view)
+
+        buttonClick.setOnClickListener {
+            val randomIndex = (0..images!!.size).random()
+            val inputStream = assets.open("images/" + images[randomIndex])
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            imageView.setImageBitmap(bitmap)
+            inputStream.close()
+        }
     }
 }
